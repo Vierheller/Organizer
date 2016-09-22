ca65abcf7c3eaf45c99091bc185473f11ced61b2
Vierheller <j.vierheller@outlook.de>
2016-09-22 18:48:54 +0200

    Added Month View

--- app/src/main/java/de/grau/organizer/MonthFragment.java
+++ app/src/main/java/de/grau/organizer/MonthFragment.java
@@ -1,12 +1,13 @@
 package de.grau.organizer;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 
 
 /**
@@ -18,6 +19,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MonthFragment extends Fragment {
+    private static final String LOG_TAG = MonthFragment.class.getCanonicalName();
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -26,6 +28,8 @@ public class MonthFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private CalendarView mCalendarView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,7 +68,10 @@ public class MonthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_month, container, false);
+        View view = inflater.inflate(R.layout.fragment_month, container, false);
+        mCalendarView = (CalendarView) view.findViewById(R.id.calendarView);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
