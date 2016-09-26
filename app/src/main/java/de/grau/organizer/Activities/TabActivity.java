439cdb1f3eb50b5a40f02e4e1d0e80ba55617e6c
Vierheller <j.vierheller@outlook.de>
2016-09-26 17:16:07 +0200

    Added Fragments

--- app/src/main/java/de/grau/organizer/Activities/TabActivity.java
+++ app/src/main/java/de/grau/organizer/Activities/TabActivity.java
@@ -1,4 +1,4 @@
-package de.grau.organizer;
+package de.grau.organizer.Activities;
 
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -18,6 +18,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import de.grau.organizer.Fragments.MonthFragment;
+import de.grau.organizer.R;
 
 public class TabActivity extends AppCompatActivity {
 
--- app/src/main/java/de/grau/organizer/Fragments/ListFragment.java
+++ app/src/main/java/de/grau/organizer/Fragments/ListFragment.java
@@ -1,25 +1,24 @@
-package de.grau.organizer;
+package de.grau.organizer.Fragments;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
 
+import de.grau.organizer.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MonthFragment.OnFragmentInteractionListener} interface
+ * {@link ListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MonthFragment#newInstance} factory method to
+ * Use the {@link ListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MonthFragment extends Fragment {
-    private static final String LOG_TAG = MonthFragment.class.getCanonicalName();
+public class ListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,11 +28,9 @@ public class MonthFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private CalendarView mCalendarView;
-
     private OnFragmentInteractionListener mListener;
 
-    public MonthFragment() {
+    public ListFragment() {
         // Required empty public constructor
     }
 
@@ -43,11 +40,11 @@ public class MonthFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MonthFragment.
+     * @return A new instance of fragment ListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MonthFragment newInstance(String param1, String param2) {
-        MonthFragment fragment = new MonthFragment();
+    public static ListFragment newInstance(String param1, String param2) {
+        ListFragment fragment = new ListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -68,10 +65,7 @@ public class MonthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_month, container, false);
-        mCalendarView = (CalendarView) view.findViewById(R.id.calendarView);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_list, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -81,7 +75,7 @@ public class MonthFragment extends Fragment {
         }
     }
 
-    /*@Override
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
@@ -90,7 +84,7 @@ public class MonthFragment extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
-    }*/
+    }
 
     @Override
     public void onDetach() {
