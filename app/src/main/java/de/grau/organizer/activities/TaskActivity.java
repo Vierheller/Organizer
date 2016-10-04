440b08a02d7ac820316336307c973e2b3270b69d
Vierheller <j.vierheller@outlook.de>
2016-10-04 20:44:47 +0200

    Shows Event in TaskActivity - not yet tested!

--- app/src/main/java/de/grau/organizer/activities/TaskActivity.java
+++ app/src/main/java/de/grau/organizer/activities/TaskActivity.java
@@ -1,5 +1,6 @@
 package de.grau.organizer.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -19,11 +20,14 @@ public class TaskActivity extends AppCompatActivity {
     public static final String DEBUG_LOG = TaskActivity.class.getCanonicalName();
     public static final String INTENT_PARAM_ID = "idParameter";
 
-    private TextView lable_title;
+    private TextView tv_title;
     private Button btn_executeAction;
+    private Button btn_edit;
 
     private EventsManager eventsManager;
     private Event mEvent;
+
+    private Context mActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,18 +45,44 @@ public class TaskActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mActivity = this;
+
         initializeGUI();
 
         eventsManager = new EventsManagerRealm();
+        eventsManager.open(this);
+
+        mEvent = getEventFromIntent(getIntent());
+
+        //If there was a fail while getting Event, do not fill UI
+        if(mEvent==null)
+            return;
+
+        fillGUI();
 
     }
 
     private void initializeGUI() {
+        tv_title = (TextView) findViewById(R.id.task_tv_title);
+        btn_edit = (Button) findViewById(R.id.task_btn_edit);
+        btn_executeAction = (Button) findViewById(R.id.task_btn_executeaction);
 
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mActivity, EditorActivity.class);
+                intent.putExtra(EditorActivity.INTENT_PARAM_EVENTID, mEvent.getId());
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void fillGUI(){
+        tv_title.setText(mEvent.getName());
     }
 
     private Event getEventFromIntent(Intent intent){
-        //Get the EventId from the previous activity
+        //Get the EventId from the previous mActivity
         String id = intent.getStringExtra(INTENT_PARAM_ID);
         //If EventId is null, tell Activity that there is a mistake
         if(id == null)
@@ -75,6 +105,7 @@ public class TaskActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         eventsManager.open(this);
+
     }
 
     @Override
