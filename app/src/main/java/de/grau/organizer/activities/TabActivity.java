9ab4521ee06c12eaf5014b128b908bf3fab5633b
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2016-10-12 00:02:56 +0200

    Made TabActivity nonswipeable - need to dicuss

--- app/src/main/java/de/grau/organizer/activities/TabActivity.java
+++ app/src/main/java/de/grau/organizer/activities/TabActivity.java
@@ -8,21 +8,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SearchEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -31,10 +25,8 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import java.util.List;
 
 import de.grau.organizer.EventsManagerRealm;
-import de.grau.organizer.fragments.ListFragment;
-import de.grau.organizer.fragments.MonthFragment;
+import de.grau.organizer.adapters.SectionsPagerAdapter;
 import de.grau.organizer.R;
-import de.grau.organizer.fragments.WeekFragment;
 import de.grau.organizer.interfaces.EventsManager;
 
 public class TabActivity extends AppCompatActivity {
@@ -74,6 +66,7 @@ public class TabActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
@@ -178,84 +171,6 @@ public class TabActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            switch(position){
-                case 0:
-                    return MonthFragment.newInstance("String1", "String2");
-                case 1:
-                    return WeekFragment.newInstance("String1", "String2");
-                case 2:
-                     return ListFragment.newInstance("String1","String2");
-                default:
-                    return PlaceholderFragment.newInstance(position + 1);
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "MONTH";
-                case 1:
-                    return "WEEK";
-                case 2:
-                    return "LIST";
-            }
-            return null;
-        }
-    }
 }
---
+++ app/src/main/java/de/grau/organizer/adapters/SectionsPagerAdapter.java
@@ -0,0 +1,66 @@
+package de.grau.organizer.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import de.grau.organizer.fragments.ListFragment;
+import de.grau.organizer.fragments.MonthFragment;
+import de.grau.organizer.fragments.WeekFragment;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private boolean isSwipeEnabled = true;
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch(position){
+            case 0:
+                return MonthFragment.newInstance("String1", "String2");
+            case 1:
+                return WeekFragment.newInstance("String1", "String2");
+            case 2:
+                 return ListFragment.newInstance("String1","String2");
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "MONTH";
+            case 1:
+                return "WEEK";
+            case 2:
+                return "LIST";
+        }
+        return null;
+    }
+
+
+
+    private void disableSwipe(){
+        this.isSwipeEnabled = false;
+    }
+
+    private void enableSwipe(){
+        this.isSwipeEnabled = true;
+    }
+}
