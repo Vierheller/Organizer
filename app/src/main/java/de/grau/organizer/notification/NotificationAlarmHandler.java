e7bcb91d64bb2c6a9e6bd8a8c4bc27049974b178
Vierheller <j.vierheller@outlook.de>
2016-10-18 20:44:10 +0200

    Changed EditorActivity to correctly pick Notification Time and handle this value in Database. Some refactoring

--- app/src/main/java/de/grau/organizer/notification/NotificationAlarmHandler.java
+++ app/src/main/java/de/grau/organizer/notification/NotificationAlarmHandler.java
@@ -21,25 +21,42 @@ public class NotificationAlarmHandler {
     public static void setNotification(Context activityContext, Event event){
         AlarmManager alarmManager = (AlarmManager)activityContext.getSystemService(Context.ALARM_SERVICE);
 
-        //Set an intent to define the target and set necessary parameters
-        Intent intent = new Intent(activityContext, NotificationBroadcastReceiver.class);
-        intent.putExtra(NotificationBroadcastReceiver.INTENT_PARAM_EVENT_ID, event.getId());
+        Intent notificationIntent = createIntentByEvent(activityContext, event);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(activityContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = createPendingIntentWithIntent(activityContext, notificationIntent);
 
-        //Get start if event
+        //Get start time of the event
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(event.getStart());
 
         //Add negative interval to the start time to get the time when notification should fire
         calendar.add(Calendar.MINUTE, (-1)*event.getNotificationTime());
 
-        Log.d(DEBUG_TAG, "Set alarm at: "+calendar.toString());
+        Log.d(DEBUG_TAG, "Set alarm for event "+event.getName()+" with id "+event.getId()+" at: "+calendar.toString());
 
+        //actually setting the systemevent
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
     public static void cancelNotification(Context activityContext, Event event) {
-        //TODO Cancel
+        AlarmManager alarmManager = (AlarmManager)activityContext.getSystemService(Context.ALARM_SERVICE);
+
+        //pendingIntent must be exactly the same like the pendingIntent which was fired
+        Intent intent = createIntentByEvent(activityContext, event);
+        PendingIntent pendingIntent = createPendingIntentWithIntent(activityContext, intent);
+
+        //Cancels a system event and does nothing if there was no notification
+        alarmManager.cancel(pendingIntent);
+    }
+
+    private static Intent createIntentByEvent(Context context, Event event){
+        //Set an intent to define the target and set necessary parameters
+        Intent intent = new Intent(context, NotificationBroadcastReceiver.class);
+        intent.putExtra(NotificationBroadcastReceiver.INTENT_PARAM_EVENT_ID, event.getId());
+        return intent;
+    }
+
+    private static PendingIntent createPendingIntentWithIntent(Context context, Intent intent){
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
